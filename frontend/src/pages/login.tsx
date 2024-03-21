db69c28da8b5d4e6c18a74bc1114e20ebe874ed5
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-03-21 12:56:34 +0800

    add login form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,59 @@
+import { Formik, Form } from "formik"
+import * as Yup from "yup"
+import logo from '../assets/MoviesWrapped_Logo-Solid.png'
+import TextInput from "../components/TextInput";
+
+const loginSchema = Yup.object().shape({
+    username: Yup.string()
+        .required("Username is a required field")
+        .min(3, "Username must be at least 3 characters"),
+    password: Yup.string()
+        .required("Password is a required field")
+        .min(8, "Password must be at least 8 characters"),
+});
+
 const Login = () => {
     return (
-        <div>
-            Login page
+        <div className="bg-base-green min-h-screen">
+            <div className='flex justify-start py-5 px-10 gap-8'>
+                <a href='/'>
+                    <img src={logo} alt='movies wrapped logo' className='object-contain max-h-10 max-w-15 transition duration-300 hover:filter hover:drop-shadow-lg' />
+                </a>
+            </div>
+            <div className="flex flex-col justify-center items-center p-10">
+                <h1 className="font-sans lg:text-lg py-3 md:text-sm sm:text-xs text-light-green text-center">Log in to Movies Wrapped</h1>
+                <Formik
+                    validationSchema={loginSchema}
+                    initialValues={{ username: "", password: "" }}
+                    onSubmit={(values) => {
+                        alert(JSON.stringify(values));
+                    }}
+                >
+                    <Form>
+                        <TextInput
+                            label='Username'
+                            name='username'
+                            type='text'
+                            placeholder='username'
+                        />
+                        <TextInput
+                            label='Password'
+                            name='password'
+                            type='text'
+                            placeholder='password'
+                        />
+                        <div className="flex gap-3 justify-center items-center pt-10 p-5">
+                            <button type="submit" className="bg-pink hover:bg-base-green hover:border hover:border-pink w-80 py-2 rounded font-roboto-bold font-bold lg:text-md text-light-green md:text-sm sm:text-xs">
+                                Log in
+                            </button>
+                        </div>
+                    </Form>
+                </Formik>
+                <div className="flex gap-3 justify-center items-center p-3">
+                    <p className="font-sans text-sm text-light-green">Don't have an account?</p>
+                    <a href='/signup' className="font-sans font-bold text-sm text-light-green hover:text-pink">Sign up</a>
+                </div>
+            </div>
         </div>
     )
 }
