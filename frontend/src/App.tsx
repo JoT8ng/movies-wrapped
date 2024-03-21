1a9799c57abd18ac8677ddbe55a13055f36ff147
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-03-21 13:03:35 +0800

    add sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Footer from './components/footer'
 import Contact from './pages/contact'
+import Signup from './pages/signup'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer />
