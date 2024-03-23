5a621630f433a0737a0e2992dcf15c7fdb5d4eec
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-03-23 12:35:07 +0800

    fix env config

--- frontend/src/services/tmdbService.tsx
+++ frontend/src/services/tmdbService.tsx
@@ -1,10 +1,9 @@
 import axios from 'axios'
+import config from '../utils/config'
 import { TrendingResultsMovie } from '../types/trending'
 
-const backendUrl = 'http://localhost:3000'
-
 const getTrendingMovies = () => {
-    const request = axios.get<TrendingResultsMovie>(`${backendUrl}/tmdb/trending/movies`)
+    const request = axios.get<TrendingResultsMovie>(`${config.BACKEND_URL}/tmdb/trending/movies`)
     return request.then(response => {
 		console.log('received trending movies data from backend')
 		return response.data
---
+++ frontend/src/utils/config.tsx
@@ -0,0 +1,5 @@
+const BACKEND_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL
+
+export default {
+    BACKEND_URL
+  };
