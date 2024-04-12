c173b559eb58cfe44c5c583b76e550532ef7343a
Jocelyn Tang <jocelynt8ng@gmail.com>
2024-04-12 11:56:32 +0800

    add piechart

---
+++ frontend/src/components/pichart.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from 'react'
+import { Chart as ChartJS, ArcElement, Title, Legend, Tooltip } from 'chart.js'
+import { Pie } from 'react-chartjs-2'
+import { WatchlistType } from '../types/watchlist'
+import { ChartProps } from 'react-chartjs-2'
+
+ChartJS.register(
+	ArcElement,
+	Title,
+	Legend,
+	Tooltip
+)
+
+const options: ChartProps<'pie'>['options'] = {
+	responsive: true,
+	plugins: {
+		legend: {
+			position: 'top',
+		},
+		title: {
+			display: true,
+			text: 'Movie Genres',
+		},
+		tooltip: {
+			mode: 'index',
+			intersect: false
+		},
+	},
+}
+
+const GENRE: string[] = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'TV Movie', 'War', 'Western']
+
+interface PieData {
+    [key: string]: number;
+    Action: number;
+    Adventure: number;
+    Animation: number;
+    Comedy: number;
+    Crime: number;
+    Documentary: number;
+    Drama: number;
+    Family: number;
+    Fantasy: number;
+    History: number;
+    Horror: number;
+    Music: number;
+    Mystery: number;
+    Romance: number;
+    Science_Fiction: number;
+    Thriller: number;
+    TV_Movie: number;
+    War: number;
+    Western: number;
+}
+
+const Piechart: React.FC<{ totalYear: WatchlistType[] }> = ({ totalYear }) => {
+    const [chartData, setChartData] = useState<PieData>({
+        Action: 0,
+        Adventure: 0,
+        Animation: 0,
+        Comedy: 0,
+        Crime: 0,
+        Documentary: 0,
+        Drama: 0,
+        Family: 0,
+        Fantasy: 0,
+        History: 0,
+        Horror: 0,
+        Music: 0,
+        Mystery: 0,
+        Romance: 0,
+        Science_Fiction: 0,
+        Thriller: 0,
+        TV_Movie: 0,
+        War: 0,
+        Western: 0,
+    })
+
+    useEffect(() => {
+        const updatedChartData = { ...chartData };
+
+        totalYear.forEach((item) => {
+            item.genres.forEach((genre) => {
+                const genreName = genre.name.replace(/ /g, '_')
+                if (updatedChartData[genreName] !== undefined) {
+                    updatedChartData[genreName]++;
+                }
+            });
+        });
+
+        setChartData(updatedChartData);
+    }, [totalYear])
+
+    const newChartData = {
+		labels: GENRE,
+		datasets: [
+			{
+				label: 'Genre',
+				data: Object.values(chartData),
+				backgroundColor: [
+                    'rgb(255, 204, 204)',
+                    'rgb(255, 229, 204)',
+                    'rgb(255, 255, 204)',
+                    'rgb(204, 255, 204)',
+                    'rgb(204, 229, 255)',
+                    'rgb(255, 204, 255)',
+                    'rgb(255, 230, 230)',
+                    'rgb(255, 242, 204)',
+                    'rgb(255, 255, 229)',
+                    'rgb(204, 255, 255)',
+                    'rgb(242, 204, 255)',
+                    'rgb(230, 255, 204)',
+				],
+			},
+		],
+	}
+
+    return (
+        <div className='flex'>
+            <Pie data-testid='piechart' data={newChartData} options={options} />
+        </div>
+    )
+}
+
+export default Piechart
--- frontend/src/components/summary.tsx
+++ frontend/src/components/summary.tsx
@@ -2,8 +2,37 @@ import middleware from '../utils/middleware'
 import { useEffect, useState, ChangeEvent } from 'react'
 import { WatchlistType } from '../types/watchlist'
 import watchlistService from '../services/watchlistService'
+import { Chart as ChartJS, ArcElement, Title, Legend, Tooltip } from 'chart.js'
+import { Pie } from 'react-chartjs-2'
+import { ChartProps } from 'react-chartjs-2'
+/*import Piechart from './pichart'*/
 
 const YEAR: number[] = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+ChartJS.register(
+	ArcElement,
+	Title,
+	Legend,
+	Tooltip
+)
+
+const options: ChartProps<'pie'>['options'] = {
+	responsive: true,
+	plugins: {
+		legend: {
+			position: 'top',
+		},
+		title: {
+			display: true,
+			text: 'Watchlist Summmary',
+            color: 'rgb(226, 235, 193)',
+		},
+		tooltip: {
+			mode: 'index',
+			intersect: false
+		},
+	},
+}
 
 const Summary = () => {
     const [userWatchlistData, setUserWatchlistData] = useState<WatchlistType[]>([])
@@ -12,6 +41,9 @@ const Summary = () => {
     const [selectedYear, setSelectedYear] = useState<number>(2024)
     const [moviesYear, setMoviesYear] = useState<WatchlistType[]>([])
     const [tvYear, setTvYear] = useState<WatchlistType[]>([])
+    const [movieLength, setMovieLength] = useState<number>(0)
+    const [tvLength, setTVLength] = useState<number>(0)
+    /*const [totalYear, setTotalYear] = useState<WatchlistType[]>([])*/
 
     const token: string | null = middleware.getToken()
     const userID: string | null = middleware.getUserID()
@@ -42,22 +74,48 @@ const Summary = () => {
         const tvYear = userWatchlistData.filter(item => item.media_type === 'tv' && new Date(item.date_watched).getFullYear() === selectedYear)
         setMoviesYear(mYear)
         setTvYear(tvYear)
+        setMovieLength(mYear.length)
+        setTVLength(tvYear.length)
+
+        // Total/all data filtered by year
+        /* const total = userWatchlistData.filter(item => new Date(item.date_watched).getFullYear() === selectedYear)
+        setTotalYear(total) */
     }, [userWatchlistData, selectedYear])
 
     const handleYearChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setSelectedYear(parseInt(event.target.value))
     }
 
+    const chartData = {
+		Movies: movieLength,
+		'TV Shows': tvLength,
+	}
+
+    const newChartData = {
+		labels: ['Movies', 'TV Shows'],
+		datasets: [
+			{
+				label: 'Media',
+				data: Object.values(chartData),
+				backgroundColor: [
+                    'rgb(226, 235, 193)',
+                    'rgb(239, 86, 87)',
+				],
+                borderWidth: 0,
+			},
+		],
+	}
+
     return (
         <div className='flex flex-col gap-5'>
             <div className='flex flex-col'>
-                <p className="font-sans text-sm text-light-green pb-3">
+                <p className="font-mono text-sm text-light-green pb-3">
                     In total you have watched...
                 </p>
-                <p className="font-sans text-sm text-light-green">
+                <p className="font-mono text-sm text-light-green">
                     <span className='text-pink'>{moviesData.length}</span> movies
                 </p>
-                <p className="font-sans text-sm text-light-green">
+                <p className="font-mono text-sm text-light-green">
                     <span className='text-pink'>{tvData.length}</span> shows
                 </p>
             </div>
@@ -70,15 +128,22 @@ const Summary = () => {
                         </option>
                     ))}
                 </select>
-                <p className="font-sans text-sm text-light-green pb-3 pt-3">
+                <p className="font-mono text-sm text-light-green pb-3 pt-3">
                     In <span className='text-pink'>{selectedYear}</span> you have watched...
                 </p>
-                <p className="font-sans text-sm text-light-green">
+                <p className="font-mono text-sm text-light-green">
                     <span className='text-pink'>{moviesYear.length}</span> movies
                 </p>
-                <p className="font-sans text-sm text-light-green">
+                <p className="font-mono text-sm text-light-green">
                     <span className='text-pink'>{tvYear.length}</span> shows
                 </p>
+            </div>
+            <div className='flex flex-col'>
+                <p className="font-mono text-sm text-light-green pb-3 pt-3 text-center">
+                    <span className='text-pink'>{selectedYear}</span> Summary
+                </p>
+                {/*<Piechart totalYear={totalYear} />*/}
+                <Pie data-testid='piechart' data={newChartData} options={options} />
             </div>
         </div>
     )
